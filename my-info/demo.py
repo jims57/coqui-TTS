1db60470b66b710cbb45ebb0a5b02f7add67b683
jims57 <jims57@163.com>
2025-04-09 17:29:24 +0800

    Update demo.py

--- my-info/demo.py
+++ my-info/demo.py
@@ -16,3 +16,17 @@ if torch.cuda.is_available():
         print(f"Current GPU device: {torch.cuda.current_device()}")
     except Exception as e:
         print(f"Error initializing CUDA: {e}")
+
+# List available 🐸TTS models
+print(TTS().list_models())
+
+# Init TTS
+tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
+
+# Run TTS
+# ❗ Since this model is multi-lingual voice cloning model, we must set the target speaker_wav and language
+# Text to speech list of amplitude values as output
+wav = tts.tts(text="Hello world!", speaker_wav="my/cloning/audio.wav", language="en")
+# Text to speech to a file
+tts.tts_to_file(text="Hello world!", speaker_wav="my/cloning/audio.wav", language="en", file_path="output.wav")
+
